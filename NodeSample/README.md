87e1ee605d5ef03da356b8a20ee6fc4cd3d32255
amillan <amillan@NI-NTB-069>
2016-11-07 18:36:42 -0300

    Add project files.

---
+++ NodeSample/README.md
@@ -0,0 +1,3 @@
+﻿# NodeSample
+
+
---
+++ src/WebApiCore/Properties/PublishProfiles/WebDeploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = '5924922c-6cc3-40b6-b4b6-456668a1a5eb'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
